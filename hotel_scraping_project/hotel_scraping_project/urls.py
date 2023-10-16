619a65ce4cbb9ddbe5adf2ad1e5f690a2ff5bb21
SandeshSS23 <sandeshssahane23@gmail.com>
2023-10-16 18:56:09 +0530

    updates in files

--- hotel_scraping_project/hotel_scraping_project/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from hotels.views import hotel_search
-
-
-from hotels import views as hotelView
-
-urlpatterns = [
-    path('hotel_search', hotelView.hotel_search, name='hotel_search'),
-]
